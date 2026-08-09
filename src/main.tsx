e10ddbdeff70aef3b9b912084459c0c75a2e0765
viasrijan <viasrijan@users.noreply.github.com>
2026-08-10 03:46:17 +0530

    Add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App'
 import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter><App/></HashRouter>
+    <Analytics />
   </React.StrictMode>
 )
